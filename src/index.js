ec9f77a11a8d3823e806bf08c029a2f075f94753
Jean Broqua <js.broqua@gmail.com>
2022-01-06 17:01:23 -0300

    added message sending and rendering on index.html

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -13,7 +14,33 @@ app.use(express.static(publicDirectotyPath))
 
 io.on('connection', (socket) => {
   console.log('New socket connection')
+
   socket.emit('message', 'Welcome')
+
+  socket.broadcast.emit('message', 'A new users has joined.')
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter()
+    if (filter.isProfane(message)) {
+      // eslint-disable-next-line node/no-callback-literal
+      return callback('Profanity is not allowed')
+    }
+
+    io.emit('message', message)
+    callback()
+  })
+
+  socket.on('sendLocation', (coords, callback) => {
+    io.emit(
+      'locationMessage',
+      `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+    )
+    callback()
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left')
+  })
 })
 
 server.listen(port, () => {
